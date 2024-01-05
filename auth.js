35789cba3f789f927c0a290158d66e8a68c2777b
ThomasNoSimp <thurawaizin.conceptx@gmail.com>
2024-01-05 10:42:53 +0630

    1

--- auth.js
+++ auth.js
@@ -9,16 +9,22 @@ function login() {
     const usernameInput = document.getElementById("username").value;
     const passwordInput = document.getElementById("password").value;
 
-    if (usernameInput === storedUsername && passwordInput === storedPassword) {
-        alert("Login successful!")
+    // Retrieve user credentials from local storage
+    const users = JSON.parse(localStorage.getItem("users")) || [];
+
+    // Check if the entered credentials match any user in the array
+    const matchedUser = users.find(user => user.username === usernameInput && user.password === passwordInput);
+
+    if (matchedUser) {
+        alert("Login successful!");
         localStorage.setItem("loggedInUser", usernameInput);
         const db_button = document.getElementById("db_button");
         db_button.style.display = "block";
+    } 
+    
+    else {
+        alert("Invalid username or password.");
     }
-    else if(usernameInput === "" || passwordInput === "") {
-        alert("Please enter a valid username and password.")
-    }
-
 }
 
 document.getElementById("loginButton").addEventListener("click", login);
